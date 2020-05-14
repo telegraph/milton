6ccc1f0281cef241036c652183ec8d937c2f4fb3
Andrew Mason <andrew@coderonfire.com>
2020-05-14 11:44:39 +0100

    Inline UI sourcemap

--- build.sh
+++ build.sh
@@ -12,10 +12,17 @@ cp src/manifest.json build/manifest.json
 cat src/ui.html > build/ui.html
 
 # Compile UI Typescript to JS in temp folder
-npx esbuild src/ui.tsx --bundle --format=iife --target=es2019 '--define:process.env.NODE_ENV="development"' --jsx-factory=h --outfile=temp/ui.js
+npx esbuild src/ui.tsx --bundle --format=iife --target=es2019 --sourcemap '--define:process.env.NODE_ENV="development"' --jsx-factory=h --outfile=temp/ui.js
+
+# Inline sourcemap
+js="$(cat temp/ui.js)"
+map=$(base64 < temp/ui.js.map)
+sourcemap="//# sourceMappingURL=data:application/json;charset=utf-8;base64,${map}"
+inlined_jss="$(echo "${js/\/\/# sourceMappingURL=ui.js.map/$sourcemap}")"
 
 # Inline UI JS into UI HTML
 echo '<script>' >> build/ui.html
-cat temp/ui.js >> build/ui.html
+echo "$inlined_jss" >> build/ui.html
 echo '</script>' >> build/ui.html
+
 
