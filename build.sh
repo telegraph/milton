ad1de70eee7013b4a013f6a5cd80e3fa4beeec6c
Andrew Mason <andrew@coderonfire.com>
2020-05-20 11:12:45 +0100

    Update build process and pacakges

--- build.sh
+++ build.sh
@@ -1,40 +1,24 @@
 #!/bin/bash
 
-# Remove old temp folder
-rm -rf temp
-mkdir temp
+# Recreate build folder
+rm -rf build
+mkdir build
 
 # Compile main code to JS int build folder
-npx esbuild src/index.tsx --bundle --format=iife --target=es2019 '--define:process.env.NODE_ENV="development"' --jsx-factory=preact.h --jsx-fragment=preact.Fragment --outfile=build/figma2html.js
-
-# Inline sourcemap
-# NOTE: Looks like Figma strips comments out of the main JS file
-index_map=$(base64 < build/figma2html.js.map)
-index_sourcemap="//# sourceMappingURL=data:application/json;charset=utf-8;base64,${index_map}"
-echo "$index_sourcemap" >> build/figma2html.js
+npx esbuild src/index.tsx --bundle --format=iife --target=es2019 \
+  --sourcemap=inline '--define:process.env.NODE_ENV="development"' \
+  --jsx-factory=preact.h --jsx-fragment=preact.Fragment \
+  --outfile=build/figma2html.js
 
 # Copy plug-in assets to build folder
-cp src/manifest.json build/manifest.json
-cat src/ui.html > build/ui.html
+cp -f src/manifest.json build/manifest.json
+cp -f src/ui.html build/ui.html
 
-# Compile UI Typescript to JS in temp folder
-npx esbuild src/ui.tsx --bundle --format=iife --target=es2019 --sourcemap '--define:process.env.NODE_ENV="development"' --jsx-factory=h --outfile=temp/ui.js
+# Compile UI Typescript to JS and store in variable
+ui_js=`npx esbuild src/ui.tsx --bundle --format=iife --target=es2019 \
+  --sourcemap=inline '--define:process.env.NODE_ENV="development"'\
+  --jsx-factory=h`
 
-# Inline sourcemap
-js="$(cat temp/ui.js)"
-map=$(base64 < temp/ui.js.map)
-sourcemap="//# sourceMappingURL=data:application/json;charset=utf-8;base64,${map}"
-inlined_js="$(echo "${js/\/\/# sourceMappingURL=ui.js.map/$sourcemap}")"
-
-# Inline UI JS into UI HTML
-echo '<script>' >> build/ui.html
-echo "$inlined_js" >> build/ui.html
-echo '</script>' >> build/ui.html
-
-# Inline UI CSS into UI HTML
+# Inline UI JS & CSS into UI HTML
 css="$(cat src/ui.css)"
-echo '<style>' >> build/ui.html
-echo "$css" >> build/ui.html
-echo '</style>' >> build/ui.html
-
-
+echo "<script>$ui_js</script>\n<style>$css</style>" >> build/ui.html
