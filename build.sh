20b5b319137ae8c71bf966f6cf8eb13a60486ef4
Andrew Mason <andrew@coderonfire.com>
2020-05-21 12:14:28 +0100

    Reduce built output target to es2017

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm -rf build
 mkdir build
 
 # Compile main code to JS int build folder
-npx esbuild src/index.tsx --bundle --format=iife --target=es2019 \
+npx esbuild src/index.tsx --bundle --format=iife --target=es2017 \
   --sourcemap=inline '--define:process.env.NODE_ENV="development"' \
   --loader:.css=text \
   --jsx-factory=preact.h --jsx-fragment=preact.Fragment \
@@ -16,7 +16,7 @@ cp -f src/manifest.json build/manifest.json
 cp -f src/ui.html build/ui.html
 
 # Compile UI Typescript to JS and store in variable
-ui_js=`npx esbuild src/ui.tsx --bundle --format=iife --target=es2019 \
+ui_js=`npx esbuild src/ui.tsx --bundle --format=iife --target=es2017 \
   --loader:.css=text \
   --sourcemap=inline '--define:process.env.NODE_ENV="development"'\
   --jsx-factory=h`
