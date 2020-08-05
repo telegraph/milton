d6e16296823e5f47dd85efb7c9ecda14168accc5
Andrew Mason <andrew@coderonfire.com>
2020-08-05 16:03:45 +0100

    Add responsive breakpoint preview input

--- src/components/ResponsiveView.tsx
+++ src/components/ResponsiveView.tsx
@@ -1,15 +1,55 @@
 import { h, Component, createRef, RefObject } from "preact";
+import debounce from "lodash/debounce";
 import { AppState } from "types";
+import { takeWhile } from "lodash";
 
 interface ResponsiveViewProps extends Pick<AppState, "svgMarkup"> {
   frameWidths: number[];
 }
 
-export class ResponsiveView extends Component<ResponsiveViewProps> {
+export class ResponsiveView extends Component<
+  ResponsiveViewProps,
+  { widthIndex: number; iframeWidth: number }
+> {
   iframeEl: RefObject<HTMLIFrameElement> = createRef();
+  inputEl: RefObject<HTMLInputElement> = createRef();
+  iframeObserver: MutationObserver | undefined;
 
-  state = {
-    widthIndex: 0,
+  constructor(props: ResponsiveViewProps) {
+    super(props);
+    const { frameWidths } = props;
+
+    this.state = {
+      widthIndex: 0,
+      iframeWidth: frameWidths[0],
+    };
+  }
+
+  componentDidMount(): void {
+    if (this.iframeEl.current) {
+      this.iframeObserver = new MutationObserver(
+        debounce(this.handleResize, 50)
+      );
+
+      this.iframeObserver.observe(this.iframeEl.current, {
+        attributes: true,
+        childList: false,
+        subtree: false,
+      });
+    }
+  }
+
+  componentWillUnmount(): void {
+    this.iframeObserver?.disconnect();
+  }
+
+  handleResize = (mutList: MutationRecord[]): void => {
+    for (const { attributeName, type, target } of mutList) {
+      if (type === "attributes" && attributeName === "style") {
+        const { width } = (target as HTMLIFrameElement).getBoundingClientRect();
+        this.setState({ iframeWidth: width });
+      }
+    }
   };
 
   setIndex = (i: number): void => {
@@ -21,25 +61,47 @@ export class ResponsiveView extends Component<ResponsiveViewProps> {
     }
   };
 
+  handleInputChange = (): void => {
+    if (this.inputEl.current && this.iframeEl.current) {
+      const { value } = this.inputEl.current;
+      this.iframeEl.current.style.width = `${value}px`;
+    }
+  };
+
   render(): h.JSX.Element {
-    const { widthIndex } = this.state;
+    const { widthIndex, iframeWidth } = this.state;
     const { svgMarkup, frameWidths } = this.props;
-    const maxWidth = Math.max(...frameWidths);
 
     return (
       <div className="f2h__responsive_preview">
-        {frameWidths.map((width, i) => (
-          <p
-            key={width}
-            className={`responsive_width_btn ${
-              widthIndex === i ? "active" : ""
-            }`}
-            onClick={() => this.setIndex(i)}
-            style={`width: ${(width / maxWidth) * 100}%`}
-          >
-            {width}
+        <div className="f2h__responsive_info">
+          <p className="f2h__responsive_width">
+            <input
+              type="number"
+              min="1"
+              value={iframeWidth}
+              ref={this.inputEl}
+              onChange={this.handleInputChange}
+              className="f2h__responsive_input"
+            />
+            px
           </p>
-        ))}
+
+          <p className="f2h__responsive_breakpoints">
+            Breakpoints
+            {frameWidths.map((width, i) => (
+              <span
+                key={width}
+                className={`responsive_width_btn ${
+                  widthIndex === i ? "active" : ""
+                }`}
+                onClick={() => this.setIndex(i)}
+              >
+                {width}
+              </span>
+            ))}
+          </p>
+        </div>
 
         <div className="f2h__responsive__wrapper">
           <iframe
