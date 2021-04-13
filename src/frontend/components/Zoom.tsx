7c6d487ccac8af6f71d6ec7aa05515ea227be7b9
Andrew <andrew.mason@telegraph.co.uk>
2021-04-13 12:10:26 +0100

    Add zoom keyboard controls

--- src/frontend/components/Zoom.tsx
+++ src/frontend/components/Zoom.tsx
@@ -1,33 +1,69 @@
-import { h, JSX } from "preact";
+import { h, Component } from "preact";
 import { UI_TEXT } from "../../constants";
 import { ActionTypes, actionSetZoom } from "frontend/actions";
 import { Dropdown } from "frontend/components/dropdown/dropdown";
+
+enum KEYS {
+  ZOOM_IN = "Equal",
+  ZOOM_OUT = "Minus",
+}
 
 interface ZoomProps {
   zoom: number;
   handleChange: (action: ActionTypes) => void;
 }
 
-export function Zoom({ zoom, handleChange }: ZoomProps): JSX.Element {
-  const zoomLabel = `${(zoom * 100).toFixed(0)}%`;
-  const ZOOM_STEPS = 2;
-  const ZOOM_MIN = 0.015625;
-  const ZOOM_MAX = 100;
+export class Zoom extends Component<ZoomProps> {
+  ZOOM_STEPS = 2;
+  ZOOM_MIN = 0.015625;
+  ZOOM_MAX = 100;
 
-  const zoomOptions = [
-    { text: "Zoom in +", value: Math.min(zoom * ZOOM_STEPS, ZOOM_MAX) },
-    { text: "Zoom out -", value: Math.max(ZOOM_MIN, zoom / ZOOM_STEPS) },
-    { text: "Zoom 100%", value: 1 },
-  ];
+  handleKeyboardInput = ({ code }: KeyboardEvent) => {
+    const { handleChange, zoom } = this.props;
 
-  return (
-    <div class="zoom">
-      <Dropdown
-        label={zoomLabel}
-        handleChange={(val: number) => handleChange(actionSetZoom(val))}
-        options={zoomOptions}
-        tooltip={UI_TEXT.ZOOM_TOOLTIP}
-      />
-    </div>
-  );
+    if (code === KEYS.ZOOM_IN) {
+      const newZoom = Math.min(zoom * this.ZOOM_STEPS, this.ZOOM_MAX);
+      handleChange(actionSetZoom(newZoom));
+    }
+
+    if (code === KEYS.ZOOM_OUT) {
+      const newZoom = Math.max(this.ZOOM_MIN, zoom / this.ZOOM_STEPS);
+      handleChange(actionSetZoom(newZoom));
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyboardInput);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeyboardInput);
+  }
+
+  render() {
+    const { handleChange, zoom } = this.props;
+    const zoomLabel = `${(zoom * 100).toFixed(0)}%`;
+    const zoomOptions = [
+      {
+        text: "Zoom in +",
+        value: Math.min(zoom * this.ZOOM_STEPS, this.ZOOM_MAX),
+      },
+      {
+        text: "Zoom out -",
+        value: Math.max(this.ZOOM_MIN, zoom / this.ZOOM_STEPS),
+      },
+      { text: "Zoom 100%", value: 1 },
+    ];
+
+    return (
+      <div class="zoom">
+        <Dropdown
+          label={zoomLabel}
+          handleChange={(val: number) => handleChange(actionSetZoom(val))}
+          options={zoomOptions}
+          tooltip={UI_TEXT.ZOOM_TOOLTIP}
+        />
+      </div>
+    );
+  }
 }
