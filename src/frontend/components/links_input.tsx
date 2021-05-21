a723df910a188a36ea8b2b38fbe3e34bf7e3c9b2
Andrew <andrew.mason@telegraph.co.uk>
2021-05-21 15:15:55 +0100

    Auto-fix missing link URLs

--- src/frontend/components/links_input.tsx
+++ src/frontend/components/links_input.tsx
@@ -1,60 +1,71 @@
-import { h, FunctionComponent } from "preact";
+import { h, Component } from "preact";
 import { EMBED_PROPERTIES, UI_TEXT } from "../../constants";
 import { actionUpdateEmbedProps, ActionTypes } from "../actions";
 
-interface LinkInputProps {
+interface Props {
   sourceUrl: string;
   embedUrl: string;
   handleChange: (action: ActionTypes) => void;
 }
 
-export const LinksInput: FunctionComponent<LinkInputProps> = (
-  props: LinkInputProps
-) => {
-  const { sourceUrl, embedUrl, handleChange } = props;
+export class LinksInput extends Component<Props> {
+  private URL_VALIDATION = "https?://.*";
+  private PROTOCOL_VALIDATION = /^http?s:\/\//i;
 
-  return (
-    <div class="side_panel links">
-      <div class="side_panel__row side_panel__row--title">
-        {UI_TEXT.TITLE_LINKS}
-      </div>
+  private cleanUrl = (text: string): string => {
+    const urlText = text.trim();
 
-      <div class="side_panel__row side_panel__row--vertical">
-        <input
-          type="url"
-          pattern="https?://.*"
-          class="input input--text input--url"
-          value={embedUrl}
-          placeholder={UI_TEXT.EMBED_PROPS_URL_PLACEHOLDER}
-          id={EMBED_PROPERTIES.EMBED_URL}
-          onChange={(e) =>
-            actionUpdateEmbedProps(
-              EMBED_PROPERTIES.EMBED_URL,
-              e.currentTarget.value.trim()
-            )(handleChange)
-          }
-          data-error-text={UI_TEXT.EMBED_PROPS_INVALID_URL}
-        />
-        <span class="input__error">{UI_TEXT.EMBED_PROPS_INVALID_URL}</span>
-      </div>
+    if (urlText === "") return "";
+    if (!this.PROTOCOL_VALIDATION.test(urlText)) return "https://" + urlText;
+    return urlText;
+  };
 
-      <div class="side_panel__row side_panel__row--vertical">
-        <input
-          class="input input--text input--url"
-          type="url"
-          pattern="https?://.*"
-          placeholder={UI_TEXT.EMBED_PROPS_SOURCE_URL_PLACEHOLDER}
-          value={sourceUrl}
-          id={EMBED_PROPERTIES.SOURCE_URL}
-          onChange={(e) =>
-            actionUpdateEmbedProps(
-              EMBED_PROPERTIES.SOURCE_URL,
-              e.currentTarget.value.trim()
-            )(handleChange)
-          }
-        />
-        <span class="input__error">{UI_TEXT.EMBED_PROPS_INVALID_URL}</span>
+  private handleInput = (event: h.JSX.TargetedFocusEvent<HTMLInputElement>) => {
+    const { handleChange } = this.props;
+    const { value, id } = event.currentTarget;
+    const url = this.cleanUrl(value);
+
+    actionUpdateEmbedProps(id as EMBED_PROPERTIES, url)(handleChange);
+  };
+
+  render(): h.JSX.Element {
+    const { sourceUrl, embedUrl } = this.props;
+
+    return (
+      <div class="side_panel links">
+        <div class="side_panel__row side_panel__row--title">
+          {UI_TEXT.TITLE_LINKS}
+        </div>
+
+        <div class="side_panel__row side_panel__row--vertical">
+          <input
+            type="url"
+            pattern={this.URL_VALIDATION}
+            class="input input--text input--url"
+            value={embedUrl}
+            placeholder={UI_TEXT.EMBED_PROPS_URL_PLACEHOLDER}
+            id={EMBED_PROPERTIES.EMBED_URL}
+            onBlur={this.handleInput}
+            data-error-text={UI_TEXT.EMBED_PROPS_INVALID_URL}
+            spellcheck={false}
+          />
+          <span class="input__error">{UI_TEXT.EMBED_PROPS_INVALID_URL}</span>
+        </div>
+
+        <div class="side_panel__row side_panel__row--vertical">
+          <input
+            class="input input--text input--url"
+            type="url"
+            pattern={this.URL_VALIDATION}
+            placeholder={UI_TEXT.EMBED_PROPS_SOURCE_URL_PLACEHOLDER}
+            value={sourceUrl}
+            id={EMBED_PROPERTIES.SOURCE_URL}
+            onBlur={this.handleInput}
+            spellcheck={false}
+          />
+          <span class="input__error">{UI_TEXT.EMBED_PROPS_INVALID_URL}</span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
